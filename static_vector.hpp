b20e9495e514cadfd7623c47e8244e82520eb58b
Jackson Ming Hu <huming2207@gmail.com>
2019-08-06 22:54:50 +1000

    Implement emplace at front

--- static_vector.hpp
+++ static_vector.hpp
@@ -8,12 +8,28 @@ class static_vector
 {
     private:
         typename std::aligned_storage<sizeof(T), alignof(T)>::type data[N];
-        std::size_t curr_size = 0;
+        std::size_t curr_size = 1;
+
+    private:
+        int move_right()
+        {
+            if(curr_size >= N) return -1;       // Array is full now, can't swap
+            if(curr_size - 1 == 0) return 0;    // Array is empty, no need to swap anyway
+
+            // Allocate an empty item at the end
+            new(&data[curr_size]) T();
+
+            for(std::size_t idx = curr_size; idx > 0; idx--) {
+                std::swap(data[idx], data[idx - 1]);
+            }
+
+            return 0;
+        }
 
     public:
         const T& front()
         {
-            if(curr_size < 1) {
+            if(curr_size - 1 < 1) {
                 new(&data[0]) T();
                 curr_size += 1;
             }
@@ -28,7 +44,7 @@ class static_vector
 
         const T& back()
         {
-            if(curr_size < 1) {
+            if(curr_size - 1 < 1) {
                 new(&data[0]) T();
                 curr_size += 1;
             }
@@ -104,7 +120,22 @@ class static_vector
             if(curr_size >= N) return -1; // Out of range
 
             // Construct value in memory of aligned storage, using inplace operator new
-            new(&data[curr_size]) T(std::forward<Args>(args)...);
+            new(&data[curr_size - 1]) T(std::forward<Args>(args)...);
+            ++curr_size;
+        }
+
+        /**
+        * Constructs an element in-place at front
+        * @tparam Args argument types passed in to constructor
+        * @param args arguments passed in to constructor
+        * @return 0 if success, -1 if failed
+        */
+        template<typename ...Args> int emplace(Args&&... args)
+        {
+            if(move_right() != 0) return -1;
+
+            // Construct value in memory of aligned storage, using inplace operator new
+            new(&data[0]) T(std::forward<Args>(args)...);
             ++curr_size;
         }
 
